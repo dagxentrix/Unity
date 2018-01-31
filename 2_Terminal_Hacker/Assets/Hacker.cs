3d735cd155308534878ce8a88bb707453e2ca070
dagxentrix <dagpedersen92@hotmail.com>
2018-01-31 22:34:37 +0100

    Terminal

--- 2_Terminal_Hacker/Assets/Hacker.cs
+++ 2_Terminal_Hacker/Assets/Hacker.cs
@@ -1,23 +1,34 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
 
 public class Hacker : MonoBehaviour
 {
+    string[] level1Passwords = { "hest", "katt", "hund", "dyr", "struts", "ekorn" };
+    string[] level2Passwords = { "mobil", "ipad", "pc", "tv", "skjerm", "kjøleskap" };
+    string[] level3Passwords = { "hus", "enebolig", "rekkehus", "blokk", "leilighet", "villa" };
+
+
+    // Game state
+    int level;
+    enum Screen { MainMenu, Password, Win };
+    Screen currentScreen;
+    string password;
+    int lives;
 
     // Use this for initialization
 
     void Start()
     {
-        ShowMainMenu("Hei på deg");
+        ShowMainMenu();
     }
 
-    void ShowMainMenu(string greeting)
+    void ShowMainMenu()
     {
-        int lives = 3;
+        currentScreen = Screen.MainMenu;
+        lives = 3;
         var userInput = 1;
         Terminal.ClearScreen();
-        Terminal.WriteLine(greeting);
+
         Terminal.WriteLine("Hva vil du hacke?");
         Terminal.WriteLine("Trykk 1 for å hacke mordi");
         Terminal.WriteLine("Trykk 2 for å hacke fardi");
@@ -25,43 +36,86 @@ public class Hacker : MonoBehaviour
         Terminal.WriteLine("Ditt valg: ");
     }
     // Update is called once per frame
+
+
     void OnUserInput(string input)
     {
-        print(input);
-        if (input == "1")
+        if (input == "menu")
         {
-            HackMordi();
+            ShowMainMenu();
         }
-        else if (input == "2")
+        else if (currentScreen == Screen.MainMenu)
         {
-            HackFardi();
+            RunMainMenu(input);
         }
-        else if (input == "3")
+        else if (currentScreen == Screen.Password)
         {
-            HackMeg();
-        }
-        else if(input == "menu")
-        {
-            ShowMainMenu("Hei");
-        }
-        else
-        {
-            Terminal.WriteLine("Ugyldig valg");
+            CheckPassword(input);
         }
     }
-    void HackMordi()
+
+
+
+    void RunMainMenu(string input)
     {
-        Terminal.ClearScreen();
-        Terminal.WriteLine("ZOMG :--)");
+        bool isValidLevel = (input == "1" || input == "2" || input == "3");
+        if (isValidLevel)
+        {
+            level = int.Parse(input);
+            StartGame();
+        }
     }
-    void HackFardi()
+
+    void StartGame()
     {
+        currentScreen = Screen.Password;
         Terminal.ClearScreen();
-        Terminal.WriteLine("Fardi");
+        switch (level)
+        {
+            case 1:
+                password = level1Passwords[Random.Range(0, level1Passwords.Length)];
+                break;
+            case 2:
+                password = level2Passwords[Random.Range(0, level2Passwords.Length)];
+                break;
+            case 3:
+                password = level3Passwords[Random.Range(0, level3Passwords.Length)];
+                break;
+            default:
+                Debug.LogError("Feil valg");
+                break;
+        }
+        Terminal.WriteLine("Du har valgt level " + level);
+        Terminal.WriteLine("Skriv passordet");
     }
-    void HackMeg()
+    private void CheckPassword(string input)
     {
+        {
+            if (input == password)
+            {
+                DisplayWinScreen();
+            }
+            else
+            {
+                Terminal.WriteLine("Feil");
+                lives--;
+                if (lives < 1)
+                {
+                    Terminal.WriteLine("Du døde");
+                }
+            }
+        }
+    }
+    void DisplayWinScreen()
+    {
+        currentScreen = Screen.Win;
         Terminal.ClearScreen();
-        Terminal.WriteLine("Meg");
+        ShowLevelReward();
+
+    }
+
+    private void ShowLevelReward()
+    {
+        Terminal.WriteLine("VERI GOD SOLJA");
     }
 }
